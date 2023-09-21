fedffce29a0f4c5daa34efdc37220735d975745e
Frommflin <freakyfrommelin@outlook.com>
2023-09-21 15:50:11 +0200

    Collecting data from open API

--- app/routes/beverages.js
+++ app/routes/beverages.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
 
-export default class BeveragesRoute extends Route {}
+export default class BeveragesRoute extends Route {
+  async model() {
+    const response = await fetch('https://api.punkapi.com/v2/beers');
+    let beverages = await response.json();
+    return { beverages };
+  }
+}
